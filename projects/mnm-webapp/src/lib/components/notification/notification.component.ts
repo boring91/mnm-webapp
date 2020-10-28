15319c9194c4b7a8fe1d2e66e078410d8d7e4d53
boring <boring@borings-MacBook-Pro.local>
2020-10-28 17:28:02 +1100

    [RMV] styling of the toastr notification

--- projects/mnm-webapp/src/lib/components/notification/notification.component.ts
+++ projects/mnm-webapp/src/lib/components/notification/notification.component.ts
@@ -190,7 +190,7 @@ export class NotificationComponent implements OnDestroy {
   }
 
   setAlertTimer(alert: Alert) {
-    alert.timerId = setTimeout(() => this.dismissAlert(alert.id), 3000);
+    alert.timerId = setTimeout(() => this.dismissAlert(alert.id), 300000);
   }
 
   removeAlertTimer(alert: Alert) {

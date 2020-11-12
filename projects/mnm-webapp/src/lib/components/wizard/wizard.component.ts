a6e2c7cb1c54fffc84f4703bfde9c3fe8b074a68
boring <boring@borings-MacBook-Pro.local>
2020-11-12 17:09:21 +1100

    [FIX] wizard hidden and shown events are not working

--- projects/mnm-webapp/src/lib/components/wizard/wizard.component.ts
+++ projects/mnm-webapp/src/lib/components/wizard/wizard.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {animate, keyframes, query, stagger, style, transition, trigger} from '@angular/animations';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { animate, keyframes, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'mnm-wizard',
@@ -8,20 +8,20 @@ import {animate, keyframes, query, stagger, style, transition, trigger} from '@a
   animations: [
     trigger('wizardAnimation', [
       transition('* => *', [
-        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', style({ opacity: 0 }), { optional: true }),
         query(':enter', stagger('100ms', [
           animate('100ms ease-in', keyframes([
-            style({opacity: 0.0, offset: 0.0}),
-            style({opacity: 1.0, offset: 1.0})
+            style({ opacity: 0.0, offset: 0.0 }),
+            style({ opacity: 1.0, offset: 1.0 })
           ]))
-        ]), {optional: true}),
+        ]), { optional: true }),
 
         query(':leave', stagger('100ms', [
           animate('100ms ease-in', keyframes([
-            style({opacity: 1.0, offset: 0.0}),
-            style({opacity: 0.0, offset: 1.0})
+            style({ opacity: 1.0, offset: 0.0 }),
+            style({ opacity: 0.0, offset: 1.0 })
           ]))
-        ]), {optional: true})
+        ]), { optional: true })
       ])
     ])
   ]
@@ -29,20 +29,23 @@ import {animate, keyframes, query, stagger, style, transition, trigger} from '@a
 export class WizardComponent implements OnInit {
 
   @Output() public hidden = new EventEmitter();
+  @Output() public shown = new EventEmitter();
 
   isShown = false;
 
-  constructor() {
+  public constructor() {
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
   }
 
   public show() {
     this.isShown = true;
+    this.shown.emit();
   }
 
   public hide() {
     this.isShown = false;
+    this.hidden.emit();
   }
 }
